96dd2d040febc0ec944679e18356ebafc17924d0
scalerjo <joseph.scalera1@gmail.com>
2021-09-28 20:02:24 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Home from './components/home';
+import Login from './components/login';
+import Dashboard from './components/dashboard/index.js';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/login' component={Login}/>
+            <Route exact path='/dashboard' component={Dashboard}/>
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+export default App;
